2ac0fe37edbf3f67234b7e22cd8e47e3877e35e8
Alxander <onepeoplgood@gmail.com>
2018-11-27 00:51:15 +0200

    Updates

---
+++ js/preloader.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".dws-progress-bar").circularProgress({
+        color: "aqua",
+        line_width: 15,
+        width: "350px",
+        height: "350px",
+        percent: 0,
+        counter_clockwise: false
+    }).circularProgress('animate', 100, 2500);
+});
+
+$(window).on('load', function() {
+    setTimeout(function(){
+        let $preloader = $('#preloader');
+        $preloader.delay(1800).fadeOut('slow');
+    }, 4000);
+});
